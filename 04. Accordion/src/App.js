2cb3274216a03994fad4e42093910e11c8bff2aa
Luka Sharashenidze <lsharashenidze2001@gmail.com>
2022-08-13 13:50:24 +0400

    Accordion
    Questions and answers

---
+++ 04. Accordion/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./SingleQuestion";
+
+const App = () => {
+  const [questions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map(question => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ 04. Accordion/src/SingleQuestion.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const SingleQuestion = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+};
+
+SingleQuestion.propTypes = {
+  title: PropTypes.string.isRequired,
+  info: PropTypes.string.isRequired,
+};
+
+export default SingleQuestion;
