92f601949d57cb15200323831cc222ca98af80d9
Luka Sharashenidze <lsharashenidze2001@gmail.com>
2022-08-13 13:54:25 +0400

    Sidebar Modal
    Open and close modal

---
+++ 12. Sidebar Modal/src/App.js
@@ -0,0 +1,15 @@
+import Modal from "./components/Modal";
+import Home from "./components/Home";
+import Sidebar from "./components/Sidebar";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
+};
+
+export default App;
---
+++ 12. Sidebar Modal/src/context.js
@@ -0,0 +1,44 @@
+import { useState, useEffect, useContext, createContext } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom hooks
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
